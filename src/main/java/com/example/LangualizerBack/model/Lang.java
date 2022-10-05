30b24a1170de6f19cd62b97d1b8d503d49b7edb7
Samir <bsam@bk.ru>
2022-10-05 17:11:47 +0300

    add key to deck entity

--- src/main/java/com/example/LangualizerBack/model/Lang.java
+++ src/main/java/com/example/LangualizerBack/model/Lang.java
@@ -4,11 +4,14 @@ public class Lang {
     private String language;
     private String text;
 
+    private Integer key;
+
     Lang() {}
 
-    Lang(String language, String text) {
+    Lang(String language, String text, Integer key) {
         this.language = language;
         this.text = text;
+        this.key = key;
     }
 
     public String getLanguage() {
@@ -19,11 +22,19 @@ public class Lang {
         return text;
     }
 
+    public Integer getKey() {
+        return key;
+    }
+
     public void setLanguage(String language) {
         this.language = language;
     }
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public void setKey(Integer key) {
+        this.key = key;
     }
 }
